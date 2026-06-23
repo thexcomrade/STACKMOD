ef2629d4692e5c324a0d41a67b12c38e88c0208a
DEVANARAYAN VS <93283856+thexcomrade@users.noreply.github.com>
2026-06-23 12:18:38 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,37 @@
+# i = 1
+# while(i>10):
+#   print('Deva')
+#   i = i+1
+
+
+# print('sdfghjk')
+# a=1
+# while (a<=10):
+#     print(a)
+#     a=a+1
+# a=10
+# while (a>0):
+#     print(a)
+#     a=a-1
+
+#1-10+ sum
+# a=1
+# while (a<=10):
+#   print(a)
+#   a=a+a   // 1,2,4,8
+
+# sum=0
+# a=1
+# while a<=10:
+#   sum = sum+a
+#   a=a+1
+# print(sum)
+
+#1-100,even no sum
+# sum=0
+# a=1
+# while a<=100:
+#   if (a%2==0):
+#     sum = sum+a
+#   a=a+1
+# print(sum)
