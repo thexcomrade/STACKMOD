448f02e9cc58cb4c7048423325e9c13d627dd9d3
DEVANARAYAN VS <93283856+thexcomrade@users.noreply.github.com>
2026-06-25 10:02:54 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,59 @@
+name = 'Stackup'
+# print(name)
+# print(name[:3])
+# print(name[3:])
+# print(name[1:7:2])
+# print(name[::-1])
+# print(name[0::2])
+# print(name[-4:-1])
+# print (name[-4:-1:-2])
+
+
+name = 'python'
+print(name[0:3])
+print(name[2:])
+
+x = 'computer'
+print(x[:4])
+print(x[3:])
+
+y = 'education'
+print(y[:4])
+print(y[5:])
+
+k = 'keyboard'
+print(k[1:])
+print(k[:-1])
+
+pr = 'programming'
+print(pr[3:7])
+
+p='python'
+print(p[::2])
+
+x = 'computer'
+print(x[1::2])
+
+p='python'
+print(p[::-1])
+
+k = 'keyboard'
+print(k[::-1])
+
+pr = 'programming'
+print(pr[:4][::-1])
+
+y = 'education'
+print(y[::3])   #eci *** eti in qp
+
+x = 'computer'
+print(x[2:6])
+
+k = 'keyboard'
+print(k[-5:])
+
+pr = 'programming'
+print(pr[::-1][::2])
+
+m = 'madam'
+print(m==m[::-1])
