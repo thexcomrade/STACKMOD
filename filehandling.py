4c95762169f3418b9ddf784a119e98708aa6221f
thexcomrade <devanarayanan.stackuplearing@gmail.com>
2026-09-07 23:38:17 +0530

    stackmod

--- filehandling.py
+++ filehandling.py
@@ -36,16 +36,37 @@ print("Number of words:", word_count)
 
 #to count letters from notes.txt
 
-letter_count = sum(c.isalpha() for c in content)
-print("Number of letters:", letter_count)
+letter = sum(c.isalpha() for c in content)
+print("Number of letters:", letter)
 
 # Q11. Write a program to copy the content of one file into another file.
 
 file = open("notes.txt", "r")
 content = file.read()
+file.close()
 
+file = open("copy_notes.txt", "w")
+file.write(content)
+file.close()
 
-# Q12. Write a program using with open(...) (context manager) to read a file — explain why with is
-# preferred over open()/close().
+# Q12. Write a program using with open(...) (context manager) to read a file — explain why with is preferred over open()/close().
+
+with open("notes.txt", "r") as file:
+    with open("copy_notes.txt", "w") as copy_file:
+        content = file.read()
+        copy_file.write(content)
+    
 # Q13. Write a program that reads a file and writes only lines containing a specific keyword into a new file.
+keyword = "hai"
+with open("notes.txt", "r") as file:
+    with open("keyword_lines.txt", "w") as keyword_file:
+        for line in file:
+            if keyword in line:
+                keyword_file.write(line)
+
 # Q14. Write a program to count how many times a given word appears in a file.
+file = open("notes.txt", "r")
+content = file.read()
+word_to_count = "hai"
+count = content.lower().split().count(word_to_count.lower())
+print(f"The word '{word_to_count}' appears {count} times in the file.")
