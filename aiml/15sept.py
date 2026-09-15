2b5c5621dbac6cfda7474f446c4bd4ffa8f12b69
thexcomrade <devanarayanan.stackuplearing@gmail.com>
2026-09-15 13:03:28 +0530

    numpy

---
+++ aiml/15sept.py
@@ -0,0 +1,89 @@
+import numpy as np
+
+# 23-27 | 15/09
+
+# 1. Statistical Functions – Practical Questions
+
+# Create a NumPy array containing the marks of 10 students. Find the mean of the marks.
+mark = np.array([67,75,78,80,75,76,87,80,67,70])
+print("Average:", np.mean(mark))
+
+# Create an array of student marks and calculate the sum of all marks.
+print("Sum:", np.sum(mark))
+
+# Create an array of numbers and find the median.
+print("Median:", np.median(mark))
+
+# Create an array of exam marks and calculate the standard deviation.
+print("Standard Deviation:", np.std(mark))
+
+# Create an array of employee salaries and find the minimum and maximum salary.
+salary=np.array([30000,32500,33000,45000,41000,34500])
+print("Maximum:", np.max(salary))
+print("Minimum:", np.min(salary))
+
+
+# Create an array containing repeated values and find the mode.
+from statistics import mode
+num=np.array([12,34,21,24,54,34,2,24])
+print("Mode:",mode(num))
+      
+
+# Create an array of monthly sales and calculate the variance.
+sales=np.array([30000,32500,33000,45000,41000,34500])
+print("Sales Variance:", np.var(sales))
+
+
+# 2. flatten() – Practical Questions
+
+# Create a 2D array (3×3) and convert it into a 1D array using flatten().
+
+arr1=np.array([
+    [
+        [6,2,5],
+        [5,9,2]
+    ],
+    [
+        [2,3,4],
+        [5,6,7]
+    ]
+])
+print(arr1.flatten())
+
+# Create a 2×4 array and convert it into a 1D array.
+
+arr2=np.array([
+    [6,2,5,6],
+    [5,9,2,1]
+    ])
+print(arr2.flatten())
+
+
+# 3. reshape() – Practical Questions
+# Create an array containing numbers from 1 to 12 and reshape it into a 3×4 array.
+arr3=np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+print(arr3.reshape(3,4))
+
+# Create numbers from 1 to 9 and reshape them into a 3×3 array.
+arr4=np.array([1,2,3,4,5,6,7,8,9])
+print(arr4.reshape(3,3))
+
+# Create numbers from 1 to 12 and reshape them into 2×2×3.
+arr4=np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+print(arr4.reshape(2,2,3))
+
+# Create a 1D array containing 20 values and reshape it into a 4×5 matrix.
+arr5=np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+print(arr5.reshape(4,5))
+
+# Create numbers from 1 to 24 and reshape them into a 3×4×2 array.
+
+
+# 4. transpose() – Practical Questions
+# Create a 2×3 matrix and find its transpose.
+# Create a 3×3 matrix and transpose it.
+# Create a student marks matrix with 3 students and 4 subjects. Transpose the matrix.
+# Create a 2×4 array and convert it into a 4×2 array using transpose.
+# Create a sales matrix containing 3 products and 4 months. Find its transpose.
+# Create a 3×2 matrix and find the transpose.
+# Create a 2D array and compare the original array and transposed array shapes.
--- aiml/work.py
+++ aiml/work.py
@@ -1,83 +1,91 @@
-import numpy as np
+# import numpy as np
 
-# 1. linspace() – Practical Questions
+# # 1. linspace() – Practical Questions
 
-# Create 5 equally spaced numbers between 0 and 20.
-a = np.linspace(0,20,5)
-print(a)
+# # Create 5 equally spaced numbers between 0 and 20.
+# a = np.linspace(0,20,5)
+# print(a)
 
-# Create 10 equally spaced numbers between 1 and 100.
-b = np.linspace(1,100,20)
-print(b)
+# # Create 10 equally spaced numbers between 1 and 100.
+# b = np.linspace(1,100,20)
+# print(b)
 
-# Generate 5 equally spaced decimal values between 2 and 10.
-c = np.linspace(2,10,5)
-print(c)
+# # Generate 5 equally spaced decimal values between 2 and 10.
+# c = np.linspace(2,10,5)
+# print(c)
 
-# 2. Random Integer – randint()
+# # 2. Random Integer – randint()
 
-# Generate 5 random integers between 1 and 50.
-d=np.random.randint(1,50,5)
-print(d)
+# # Generate 5 random integers between 1 and 50.
+# d=np.random.randint(1,50,5)
+# print(d)
 
-# Generate 10 random integers between 10 and 100.
-e=np.random.randint(10,100,5)
-print(e)
+# # Generate 10 random integers between 10 and 100.
+# e=np.random.randint(10,100,5)
+# print(e)
 
-# Generate 5 random numbers representing students' marks between 0 and 100.
-f=np.random.randint(0,100,5)
-print(f)
+# # Generate 5 random numbers representing students' marks between 0 and 100.
+# f=np.random.randint(0,100,5)
+# print(f)
 
-# 5. Positive Indexing in NumPy
+# # 5. Positive Indexing in NumPy
 
-# Create an array containing 10, 20, 30, 40, 50. Access the first element using its index.
-arr=([10, 20, 30, 40, 50])
-print(arr[1])
+# # Create an array containing 10, 20, 30, 40, 50. Access the first element using its index.
+# arr=([10, 20, 30, 40, 50])
+# print(arr[1])
 
-# Access the third element from the same array.
-print(arr[3])
+# # Access the third element from the same array.
+# print(arr[3])
 
-# Access the last element using positive indexing.
-print(arr[4])
+# # Access the last element using positive indexing.
+# print(arr[4])
 
-# Create an array of 6 numbers and access the element at index 3.
-r=([10, 20, 30, 40, 50,60])
-print(r[3])
+# # Create an array of 6 numbers and access the element at index 3.
+# r=([10, 20, 30, 40, 50,60])
+# print(r[3])
 
-# Create a 2D array and access the element from the second row and third column using positive indexing.
-r = np.array([
-    [10, 20, 30],
-    [40, 50,60]
-    ])
-print(r[1][2])
-
-
-
-# 6. Negative Indexing in NumPy
-
-# Create an array containing 10, 20, 30, 40, 50. Access the last element using negative indexing.
-m=np.array([10, 20, 30, 40, 50])
-print(m[-1])
-# Access the second-last element using negative indexing.
-print(m[-2])
-# Access the third-last element using negative indexing.
-print(m[-3])
+# # Create a 2D array and access the element from the second row and third column using positive indexing.
+# r = np.array([
+#     [10, 20, 30],
+#     [40, 50,60]
+#     ])
+# print(r[1][2])
 
 
-# Create an array of 6 numbers and access the last 3 elements individually using negative indexes.
-n=np.array([10, 20, 30, 40, 50,60])
-print(n[-1]), print(n[-2]), print(n[-3])
 
-# Create a 2D array and access the last element of the last row using negative indexing.
-x = np.array([
-    [10, 20, 30],
-    [40, 50,60]
-    ])
-print(x[1][-1])
+# # 6. Negative Indexing in NumPy
+
+# # Create an array containing 10, 20, 30, 40, 50. Access the last element using negative indexing.
+# m=np.array([10, 20, 30, 40, 50])
+# print(m[-1])
+# # Access the second-last element using negative indexing.
+# print(m[-2])
+# # Access the third-last element using negative indexing.
+# print(m[-3])
 
 
-#RESHAPE ARRAY
+# # Create an array of 6 numbers and access the last 3 elements individually using negative indexes.
+# n=np.array([10, 20, 30, 40, 50,60])
+# print(n[-1]), print(n[-2]), print(n[-3])
 
-y = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
-newarr = y.reshape(4, 3)
-print(newarr)
+# # Create a 2D array and access the last element of the last row using negative indexing.
+# x = np.array([
+#     [10, 20, 30],
+#     [40, 50,60]
+#     ])
+# print(x[1][-1])
+
+
+# #RESHAPE ARRAY
+
+# y = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+# newarr = y.reshape(4, 3)
+# print(newarr)
+
+
+# marks = np.array([80, 75, 90, 65, 85])
+# print("Total:", np.sum(marks))
+# print("Average:", np.mean(marks))
+# print("Maximum:", np.max(marks))
+# print("Minimum:", np.min(marks))
+# print("Standard Deviation:", np.std(marks))
